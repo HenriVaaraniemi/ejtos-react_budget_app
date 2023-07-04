14e8e187d5b8aaa3db363f02f2d92dd76f8ce9f9
HenriVaaraniemi <henri.vaaraniemi@hotmail.fi>
2023-07-04 05:40:56 -0400

    progress

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget } = useContext(AppContext); 
+    const [setBudget] = useState(1);
+    const onChangeEvent = () => {
+        const value = Number(event.target.value)
+        if(value >= 20001) {
+            
+            alert("The value cannot exceed 20000 £");
+            setBudget(20000);
+            return;
+        }
+        else {
+            setBudget(value);
+            //alert("test");
+        }
+    };
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £{<input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        defaultValue={budget}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => onChangeEvent(event.target.value)}>
+                        </input>}</span>
+                        
+            
         </div>
+        
     );
 };
 export default Budget;
